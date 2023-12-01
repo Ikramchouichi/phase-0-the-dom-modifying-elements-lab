0cafed89c44c933728f1c0383a58312747279fdb
Ikram Chouichi <ikram@netronian.com>
2023-11-30 22:49:59 -0500

    Creating and Inserting DOM Nodes Lab

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code here!
+let mainElement = document.getElementById('main');
+if (mainElement) {
+    mainElement.remove();
+}
+
+let newHeader = document.createElement('h1');
+
+newHeader.id = 'victory';
+newHeader.textContent = 'YOUR-NAME is the champion';
+
+document.body.appendChild(newHeader);
